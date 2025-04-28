ac1f4b8a0b1f4bf09dccb292ec273639842489c6
felipemoraless__ <felipemorales453@gmail.com>
2025-04-27 22:18:51 -0600

    Delete tutorial.py

--- tutorial.py
+++
@@ -1,8 +0,0 @@
-from manim import *
-
-class SquareToCircle(Scene):
-    def construct(self):
-        square = Square()
-        self.play(Create(square))
-        self.play(Transform(square, Circle()))
-        self.play(FadeOut(square))
